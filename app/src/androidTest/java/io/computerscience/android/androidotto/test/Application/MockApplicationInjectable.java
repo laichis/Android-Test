bf62deba9a968a6b8558c66471cb7f396389bf93
Chang Sean <sean@lydsec.com>
2017-06-27 12:24:23 +0800

    d
    dd

---
+++ app/src/androidTest/java/io/computerscience/android/androidotto/test/Application/MockApplicationInjectable.java
@@ -0,0 +1,46 @@
+package io.computerscience.android.androidotto.test.Application;
+
+import android.app.Application;
+import android.content.Context;
+
+import dagger.Module;
+import dagger.ObjectGraph;
+import dagger.Provides;
+import io.computerscience.android.androidotto.Interface.DaggerInjector;
+
+/**
+ * This class defined standard behaviour that DaggerInjector needs, and provides an
+ * application context for tests
+ *
+ * Define the getModules() method in an anonymous inner-class implementation defined
+ * within each Test, since not all modules will be required for all tests.
+ */
+public abstract class MockApplicationInjectable extends Application implements DaggerInjector {
+
+    //region Application Context Module
+    @Module(library = true)
+    public final class MockApplicationContextModule {
+        Context context;
+        public MockApplicationContextModule(Context app) {
+            context = app;
+        }
+
+        @Provides
+        public Context provideContext(){
+            return context;
+        }
+    }
+    //endregion
+
+    private ObjectGraph mObjectGraph;
+
+    public MockApplicationInjectable(Context context) {
+        super.attachBaseContext(context);
+        mObjectGraph = ObjectGraph.create(getModules());
+    }
+
+    @Override
+    public void inject(Object o) {
+        mObjectGraph.inject(o);
+    }
+}
